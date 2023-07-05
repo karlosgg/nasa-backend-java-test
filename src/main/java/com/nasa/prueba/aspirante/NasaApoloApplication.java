db3ad6cf46233f3932608d4222b142dadc00dbf2
Jose Grijalva <jose.grijalva@acftechnologies.com>
2023-07-05 08:38:28 -0600

    :sparkles: setting up configurations

--- src/main/java/com/nasa/prueba/aspirante/NasaApoloApplication.java
+++ src/main/java/com/nasa/prueba/aspirante/NasaApoloApplication.java
@@ -2,8 +2,12 @@ package com.nasa.prueba.aspirante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
+@EnableFeignClients
 public class NasaApoloApplication {
 
 	public static void main(String[] args) {
